5bcc909e2a6c38200bc24746259a8463300fc489
emmy grammy <ojeabuo24@gmail.com>
2026-06-20 02:05:50 +0100

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">about us</h1>
+     
+    </div>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">contact us</h1>
+     
+    </div>
+  );
+}
